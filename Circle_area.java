73574f22de894294238ad0acee3e5031c6aa7e0b
Nency Parmar <148302864+nencyp1604@users.noreply.github.com>
2024-01-06 19:06:12 +0530

    Add files via upload

---
+++ Circle_area.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Circle_area
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a value of r: ");
+        double r=sc.nextInt();
+        System.out.println((3.14*r*r));
+    }
+}
---
+++ Farenhit.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Farenhit
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a value of f: ");
+        double f=sc.nextInt();
+        double c=f-32*5/9;
+        System.out.println(c);
+    }
+}
